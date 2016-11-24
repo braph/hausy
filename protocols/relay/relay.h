f03afb014bd210af875ffd8655710a4b971125b8
braph <braph93@gmx.de>
2016-11-24 22:38:44 +0100

    changed include-define

--- protocols/relay/relay.h
+++ protocols/relay/relay.h
@@ -1,5 +1,5 @@
-#ifndef Hausy_Protocol_Relay_h
-#define Hausy_Protocol_Relay_h
+#ifndef _HAUSY_PROTOCOL_RELAY_H
+#define _HAUSY_PROTOCOL_RELAY_H
 
 
 #include "../../hausy.h"
@@ -19,15 +19,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-size_t relay_parse_state_inform(
-   hausy_bitstorage *data,
-   size_t data_size,
-   size_t at,
-   hausy_id *systemID,
-   hausy_id *unitID,
-   hausy_bool *state
-);
 
 size_t relay_create_on(
    hausy_bitstorage *data,
@@ -58,6 +49,14 @@ size_t relay_create_state_inform(
    hausy_id systemID,
    hausy_id unitID,
    hausy_bool state
+);
+size_t relay_parse_state_inform(
+   hausy_bitstorage *data,
+   size_t data_size,
+   size_t at,
+   hausy_id *systemID,
+   hausy_id *unitID,
+   hausy_bool *state
 );
 
 #ifdef __cplusplus
