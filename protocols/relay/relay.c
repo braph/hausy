30f7fbed8ec2c3844d8d42d7529de6427a956842
braph <braph93@gmx.de>
2016-11-27 04:33:56 +0100

    removed inform-commands

--- protocols/relay/relay.c
+++ protocols/relay/relay.c
@@ -3,96 +3,44 @@
 
 #include "relay.h"
 
-size_t relay_parse_state_inform
- (
-   hausy_bitstorage *data,
-   size_t data_size,
-   size_t at,
-   hausy_id *systemID,
-   hausy_id *unitID,
-   hausy_bool *state
- )
-{
-   if (data == NULL)
-      return at
-             + RELAY_CMD_STATE_INFORM_ARG1_SYSTEM_ID
-             + RELAY_CMD_STATE_INFORM_ARG2_UNIT_ID
-             + RELAY_CMD_STATE_INFORM_ARG3_STATE;
-
-   if (data_size < at
-         + RELAY_CMD_STATE_INFORM_ARG1_SYSTEM_ID
-         + RELAY_CMD_STATE_INFORM_ARG2_UNIT_ID
-         + RELAY_CMD_STATE_INFORM_ARG3_STATE
-      )
-      return 0;
-            
-   *systemID = hausy_read_long(data, RELAY_CMD_STATE_INFORM_ARG1_SYSTEM_ID, &at);
-   *unitID   = hausy_read_long(data, RELAY_CMD_STATE_INFORM_ARG2_UNIT_ID,   &at);
-   *state    = hausy_read_long(data, RELAY_CMD_STATE_INFORM_ARG3_STATE,     &at);
-
-   return at;
-}
-
 size_t relay_create_on
  (
-   hausy_bitstorage *data,
+   hausy_request *req,
    hausy_id systemID,
    hausy_id unitID
  )
 {
-   return hausy_create_command(data, RELAY_PROTOCOL_ID, systemID, unitID, RELAY_CMD_ON);
+   return hausy_create_request(req, RELAY_PROTOCOL_ID, systemID, unitID, RELAY_CMD_ON);
 }
 
 size_t relay_create_off
  (
-   hausy_bitstorage *data,
+   hausy_request *req,
    hausy_id systemID,
    hausy_id unitID
  )
 {
-   return hausy_create_command(data, RELAY_PROTOCOL_ID, systemID, unitID, RELAY_CMD_OFF);
+   return hausy_create_request(req, RELAY_PROTOCOL_ID, systemID, unitID, RELAY_CMD_OFF);
 }
 
 size_t relay_create_toggle
  (
-   hausy_bitstorage *data,
+   hausy_request *req,
    hausy_id systemID,
    hausy_id unitID
  )
 {
-   return hausy_create_command(data, RELAY_PROTOCOL_ID, systemID, unitID, RELAY_CMD_TOGGLE);
+   return hausy_create_request(req, RELAY_PROTOCOL_ID, systemID, unitID, RELAY_CMD_TOGGLE);
 }
 
 size_t relay_create_state_query
  (
-   hausy_bitstorage *data,
+   hausy_request *req,
    hausy_id systemID,
    hausy_id unitID
  )
 {
-   return hausy_create_command(data, RELAY_PROTOCOL_ID, systemID, unitID, RELAY_CMD_STATE_QUERY);
-}
-
-size_t relay_create_state_inform
- (
-   hausy_bitstorage *data,
-   hausy_id systemID,
-   hausy_id unitID,
-   hausy_bool state
- )
-{
-   if (data == NULL)
-      return hausy_create_command(NULL, 0, 0, 0, 0)
-             + RELAY_CMD_STATE_INFORM_ARG1_SYSTEM_ID
-             + RELAY_CMD_STATE_INFORM_ARG2_UNIT_ID
-             + RELAY_CMD_STATE_INFORM_ARG3_STATE;
-
-   size_t at;
-   at = hausy_create_command(data, RELAY_PROTOCOL_ID, HAUSY_BROADCAST_ID, HAUSY_BROADCAST_ID, RELAY_CMD_STATE_INFORM);
-   at = hausy_write_long(data, systemID, RELAY_CMD_STATE_INFORM_ARG1_SYSTEM_ID, at);
-   at = hausy_write_long(data, unitID,   RELAY_CMD_STATE_INFORM_ARG2_UNIT_ID,   at);
-   at = hausy_write_long(data, state,    RELAY_CMD_STATE_INFORM_ARG3_STATE,     at);
-   return at;
+   return hausy_create_request(req, RELAY_PROTOCOL_ID, systemID, unitID, RELAY_CMD_STATE_QUERY);
 }
 
 #endif // _HAUSY_PROTOCOL_RELAY_C
--- protocols/relay/relay.h
+++ protocols/relay/relay.h
@@ -1,62 +1,40 @@
 #ifndef _HAUSY_PROTOCOL_RELAY_H
 #define _HAUSY_PROTOCOL_RELAY_H
 
-
 #include "../../hausy.h"
 
-#define RELAY_PROTOCOL_ID                       53 // 'R'
-
-#define RELAY_CMD_OFF                           0  // '0'
-#define RELAY_CMD_ON                            1  // '1'
-#define RELAY_CMD_TOGGLE                        55 // 'T'
-#define RELAY_CMD_STATE_QUERY                   52 // 'Q'
-
-#define RELAY_CMD_STATE_INFORM                  44 // 'I'
-#define RELAY_CMD_STATE_INFORM_ARG1_SYSTEM_ID   HAUSY_ID_BITLENGTH
-#define RELAY_CMD_STATE_INFORM_ARG2_UNIT_ID     HAUSY_ID_BITLENGTH
-#define RELAY_CMD_STATE_INFORM_ARG3_STATE       HAUSY_BOOL_BITLENGTH
+#define RELAY_PROTOCOL_ID         53 // 'R'
+#define RELAY_CMD_OFF             0  // '0'
+#define RELAY_CMD_ON              1  // '1'
+#define RELAY_CMD_TOGGLE          55 // 'T'
+#define RELAY_CMD_STATE_QUERY     52 // 'Q'
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 size_t relay_create_on(
-   hausy_bitstorage *data,
+   hausy_request *request,
    hausy_id systemID,
    hausy_id unitID
 );
 
 size_t relay_create_off(
-   hausy_bitstorage *data,
+   hausy_request *request,
    hausy_id systemID,
    hausy_id unitID
 );
 
 size_t relay_create_toggle(
-   hausy_bitstorage *data,
+   hausy_request *request,
    hausy_id systemID,
    hausy_id unitID
 );
 
-size_t relay_create_state_query(
-   hausy_bitstorage *data,
+size_t relay_create_query(
+   hausy_request *request,
    hausy_id systemID,
    hausy_id unitID
-);
-
-size_t relay_create_state_inform(
-   hausy_bitstorage *data,
-   hausy_id systemID,
-   hausy_id unitID,
-   hausy_bool state
-);
-size_t relay_parse_state_inform(
-   hausy_bitstorage *data,
-   size_t data_size,
-   size_t at,
-   hausy_id *systemID,
-   hausy_id *unitID,
-   hausy_bool *state
 );
 
 #ifdef __cplusplus
