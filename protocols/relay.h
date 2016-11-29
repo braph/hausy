5a7417a553c98bd2b87e3d9b3f70255fb96c65e9
braph <braph93@gmx.de>
2016-11-29 05:20:22 +0100

    protocols are defined inline now

--- protocols/relay.h
+++ protocols/relay.h
@@ -1,8 +1,17 @@
-#ifndef _HAUSY_PROTOCOL_RELAY_C
-#define _HAUSY_PROTOCOL_RELAY_C
+#ifndef _HAUSY_PROTOCOL_RELAY_H
+#define _HAUSY_PROTOCOL_RELAY_H
 
-#include "relay.h"
+#define RELAY_PROTOCOL_ID     53 // 'R'
+#define RELAY_CMD_OFF         0  // '0'
+#define RELAY_CMD_ON          1  // '1'
+#define RELAY_CMD_TOGGLE      55 // 'T'
+#define RELAY_CMD_QUERY       52 // 'Q'
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static inline
 size_t relay_create_on
  (
    hausy_request *req,
@@ -13,6 +22,7 @@ size_t relay_create_on
    return hausy_create_request(req, RELAY_PROTOCOL_ID, systemID, unitID, RELAY_CMD_ON);
 }
 
+static inline
 size_t relay_create_off
  (
    hausy_request *req,
@@ -23,6 +33,7 @@ size_t relay_create_off
    return hausy_create_request(req, RELAY_PROTOCOL_ID, systemID, unitID, RELAY_CMD_OFF);
 }
 
+static inline
 size_t relay_create_toggle
  (
    hausy_request *req,
@@ -33,6 +44,7 @@ size_t relay_create_toggle
    return hausy_create_request(req, RELAY_PROTOCOL_ID, systemID, unitID, RELAY_CMD_TOGGLE);
 }
 
+static inline
 size_t relay_create_query
  (
    hausy_request *req,
@@ -43,4 +55,8 @@ size_t relay_create_query
    return hausy_create_request(req, RELAY_PROTOCOL_ID, systemID, unitID, RELAY_CMD_QUERY);
 }
 
-#endif // _HAUSY_PROTOCOL_RELAY_C
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _HAUSY_PROTOCOL_RELAY_H
